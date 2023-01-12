5778a59fa7c8025c4cdaf7d0a0d3f79938e36875
Adetoye Adewoye <71382301+adetoye-dev@users.noreply.github.com>
2023-01-12 06:44:40 +0100

    Hide waqi api key in .env file

--- src/apis/waqi.js
+++ src/apis/waqi.js
@@ -1,11 +1,9 @@
 import axios from "axios";
 
-const WAQI_API_KEY = "fd1eb45958c52fdbacbe6440c958378e3685e939";
-
 const waqi = axios.create({
   baseURL: "https://api.waqi.info/feed",
   params: {
-    token: WAQI_API_KEY,
+    token: import.meta.env.VITE_WAQI_API_KEY,
   },
 });
 
