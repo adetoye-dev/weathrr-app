f4c51b3952395293d381529aacd350cf2351362d
Adetoye Adewoye <71382301+adetoye-dev@users.noreply.github.com>
2023-01-14 15:18:47 +0100

    update post card to use props

--- src/components/posts/PostCard.jsx
+++ src/components/posts/PostCard.jsx
@@ -2,31 +2,31 @@ import "./PostCard.css";
 import postImage from "../../assets/user-image.png";
 import profileIcon from "../../assets/profile-icon.png";
 
-const PostCard = ({ title }) => {
+const PostCard = (props) => {
   return (
     <div className="post">
       <div className="post-img">
         <img src={postImage} alt="post-image" />
       </div>
-      <div className="user-info flex-center-v">
+      <div className="user-info flex-center-y">
         <div className="user-img">
           <img src={profileIcon} alt="user" />
         </div>
-        <span className="user-name">Lorem Ipsum</span>
-        <div className="weather-info flex-center-v">
-          <span className="temp">15</span>
+        <span className="user-name">{props.userName}</span>
+        <div className="weather-info flex-center-y">
+          <span className="temp">{props.temp}</span>
           <span className="icon">
             <img src="./icons/clear sky.svg" />
           </span>
         </div>
       </div>
-      <div className="post-info flex-center-v">
-        <h5 className="post-title">{title}</h5>
-        <div className="location-info flex-center-v">
+      <div className="post-info flex-center-y">
+        <h5 className="post-title">{props.title}</h5>
+        <div className="location-info flex-center-y">
           <span className="icon">
             <img src="./icons/location-icon.svg" alt="location-icon" />
           </span>
-          <span className="location">New York</span>
+          <span className="location">{props.location}</span>
         </div>
       </div>
     </div>
