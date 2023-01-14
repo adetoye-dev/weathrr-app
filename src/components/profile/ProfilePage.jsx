a0df0c87b8ec31e82746896396ac48db3c69694b
Adetoye Adewoye <71382301+adetoye-dev@users.noreply.github.com>
2023-01-14 13:29:36 +0100

    Move masonry post cards from profile  to home page

--- src/components/profile/ProfilePage.jsx
+++ src/components/profile/ProfilePage.jsx
@@ -1,31 +1,7 @@
-import PostCard from "../post/PostCard";
-import Masonry from "react-masonry-css";
-
 const ProfilePage = () => {
-  const breakpoints = {
-    default: 4,
-    1100: 3,
-    700: 2,
-    500: 1,
-  };
-
   return (
     <>
-      <Masonry
-        breakpointCols={breakpoints}
-        className="my-masonry-grid"
-        columnClassName="my-masonry-grid_column"
-      >
-        <PostCard title="Between flexbox, css columns, css grid we settled on plain ol' div's and a dab of flexbox that allows for fluid responsive layouts by default but most importantly is true to Reacts rendering lifecycle." />
-        <PostCard />
-        <PostCard title="react-masonry-css Is a React Component with a simple interface to order items into the desired columns at specified breakpoints. With minimal CSS this leads to a quick, reliable solution that also has great browser support along with rendering performance." />
-        <PostCard />
-        <PostCard title="also has great browser support along with rendering performance." />
-        <PostCard />
-        <PostCard title="react-masonry-css Is a React Component with a simple interface to order items into the desired columns at specified breakpoints. With minimal CSS this leads to a quick, reliable solution that also has great browser support along with rendering performance." />
-        <PostCard />
-        <PostCard />
-      </Masonry>
+      <h1>Profile Page</h1>
     </>
   );
 };
