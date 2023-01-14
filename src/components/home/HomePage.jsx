ee04e6cd4a749b1836d4448ebec57b4ed9aeb72d
Adetoye Adewoye <71382301+adetoye-dev@users.noreply.github.com>
2023-01-14 15:21:23 +0100

    Render test post cards with custom props

--- src/components/home/HomePage.jsx
+++ src/components/home/HomePage.jsx
@@ -15,15 +15,60 @@ const HomePage = () => {
         className="my-masonry-grid"
         columnClassName="my-masonry-grid_column"
       >
-        <PostCard title="Between flexbox, css columns, css grid we settled on plain ol' div's and a dab of flexbox that allows for fluid responsive layouts by default but most importantly is true to Reacts rendering lifecycle." />
-        <PostCard />
-        <PostCard title="react-masonry-css Is a React Component with a simple interface to order items into the desired columns at specified breakpoints. With minimal CSS this leads to a quick, reliable solution that also has great browser support along with rendering performance." />
-        <PostCard />
-        <PostCard title="also has great browser support along with rendering performance." />
-        <PostCard />
-        <PostCard title="react-masonry-css Is a React Component with a simple interface to order items into the desired columns at specified breakpoints. With minimal CSS this leads to a quick, reliable solution that also has great browser support along with rendering performance." />
-        <PostCard />
-        <PostCard />
+        <PostCard
+          title="Sit aliquam at dui leo."
+          userName="Lorem Ipsum"
+          temp="15°"
+          location="New York"
+        />
+        <PostCard
+          title="Sit aliquam at dui leo."
+          userName="Lorem Ipsum"
+          temp="15°"
+          location="New York"
+        />
+        <PostCard
+          title="Sit aliquam at dui leo."
+          userName="Lorem Ipsum"
+          temp="15°"
+          location="New York"
+        />
+        <PostCard
+          title="Sit aliquam at dui leo."
+          userName="Lorem Ipsum"
+          temp="15°"
+          location="New York"
+        />
+        <PostCard
+          title="Sit aliquam at dui leo."
+          userName="Lorem Ipsum"
+          temp="15°"
+          location="New York"
+        />
+        <PostCard
+          title="Sit aliquam at dui leo."
+          userName="Lorem Ipsum"
+          temp="15°"
+          location="New York"
+        />
+        <PostCard
+          title="Sit aliquam at dui leo."
+          userName="Lorem Ipsum"
+          temp="15°"
+          location="New York"
+        />
+        <PostCard
+          title="Sit aliquam at dui leo."
+          userName="Lorem Ipsum"
+          temp="15°"
+          location="New York"
+        />
+        <PostCard
+          title="Sit aliquam at dui leo."
+          userName="Lorem Ipsum"
+          temp="15°"
+          location="New York"
+        />
       </Masonry>
     </>
   );
