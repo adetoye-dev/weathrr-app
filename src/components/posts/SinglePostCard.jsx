96fcf6a73c2886198dbe4f8a8516f82b3ea3c506
Adetoye Adewoye <71382301+adetoye-dev@users.noreply.github.com>
2023-01-17 16:44:23 +0100

    use custom state props

--- src/components/posts/SinglePostCard.jsx
+++ src/components/posts/SinglePostCard.jsx
@@ -1,29 +1,31 @@
 import "./SinglePostCard.css";
+import { useLocation } from "react-router-dom";
 
 const SinglePostCard = () => {
+  const { state } = useLocation();
   return (
     <>
       <div className="single-post-nav"></div>
       <div className="single-post-card">
         <div className="single-post-image">
-          <img src="/images/pic1.png" alt="post-image" />
+          <img src={`/images/${state.postImage}`} alt="post-image" />
         </div>
         <div className="single-post-data">
           <div className="single-post-location-card">
             <div className="post-location-info">
               <span className="icon">
-                <img src="./icons/location-icon.svg" alt="location-icon" />
+                <img src="/icons/location-icon.png" alt="location-icon" />
               </span>
-              <span className="post-location">location</span>
+              <span className="post-location">{state.location}</span>
             </div>
             <div className="post-weather-info">
-              <span className="temp">16</span>
+              <span className="temp">{state.temp}</span>
               <span className="icon">
-                <img src="./icons/clear sky.svg" />
+                <img src="/icons/clear sky.svg" />
               </span>
             </div>
           </div>
-          <h2 className="single-post-title">Sit aliquam at dui leo.</h2>
+          <h2 className="single-post-title">{state.title}</h2>
           <p className="post-desc">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
             eiusmod tempor incididunt ut labore et doloris nisi ut aliquip ex ea
