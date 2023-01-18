3138da2b80582891a1d352274a72d42bd79c9c3f
Adetoye Adewoye <71382301+adetoye-dev@users.noreply.github.com>
2023-01-18 09:10:56 +0100

    Render profile card and user posts

--- src/components/profile/ProfilePage.jsx
+++ src/components/profile/ProfilePage.jsx
@@ -1,7 +1,30 @@
+import ProfileCard from "./ProfileCard";
+import "./ProfilePage.css";
+import MasonryLayout from "../layouts/MasonryLayout";
+import PostCard from "../posts/PostCard";
+import data from "../../../data.json";
+
 const ProfilePage = () => {
   return (
     <>
-      <h1>Profile Page</h1>
+      <ProfileCard />
+      <div className="user-posts">
+        <div className="posts-title">Posts</div>
+        <MasonryLayout>
+          {data.map((item) => {
+            return (
+              <PostCard
+                title={item.title}
+                userName={item.userName}
+                temp={item.temp}
+                location={item.location}
+                postImage={item.postImage}
+                postDesc={item.postDesc}
+              />
+            );
+          })}
+        </MasonryLayout>
+      </div>
     </>
   );
 };
