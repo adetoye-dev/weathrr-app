1791f49017cc6939e700b177a42b9cbb04606f1a
Adetoye Adewoye <71382301+adetoye-dev@users.noreply.github.com>
2023-01-18 08:52:05 +0100

    Rename layout to PageLayout and move to ./layouts

--- src/components/layouts/PageLayout.jsx
+++ src/components/layouts/PageLayout.jsx
@@ -1,8 +1,8 @@
-import PrimaryNav from "./navigations/PrimaryNav";
-import SecondaryNav from "./navigations/SecondaryNav";
+import PrimaryNav from "../../navigations/PrimaryNav";
+import SecondaryNav from "../../navigations/SecondaryNav";
 import { Outlet } from "react-router-dom";
 
-const Layout = () => {
+const PageLayout = () => {
   return (
     <>
       <PrimaryNav />
@@ -14,4 +14,4 @@ const Layout = () => {
   );
 };
 
-export default Layout;
+export default PageLayout;
