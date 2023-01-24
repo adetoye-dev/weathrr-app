f9570549e5018594ad7200235c8c9b6535ee457b
Adetoye Adewoye <71382301+adetoye-dev@users.noreply.github.com>
2023-01-24 14:32:05 +0100

    Fix nav icons disappearing on single post page

--- src/components/navigations/NavItem.jsx
+++ src/components/navigations/NavItem.jsx
@@ -2,10 +2,10 @@ import { NavLink } from "react-router-dom";
 
 const NavItem = ({ icon, text, link }) => {
   const style = {
-    background: `url(./icons/${icon}.svg) center center no-repeat`,
+    background: `url(/icons/${icon}.svg) center center no-repeat`,
   };
   const activeStyle = {
-    background: `url(./icons/${icon}-active.svg) center center no-repeat`,
+    background: `url(/icons/${icon}-active.svg) center center no-repeat`,
   };
   return (
     <div className="secondary-nav-item">
--- src/components/navigations/SecondaryNav.jsx
+++ src/components/navigations/SecondaryNav.jsx
@@ -7,7 +7,7 @@ const SecondaryNav = () => {
       <NavItem icon="home" text="home" link="/" />
       <NavItem icon="weather" text="weather" link="/weather" />
       <div className="new-post-icon">
-        <img src="./icons/plus-icon.png" alt="plus-icon" />
+        <img src="/icons/plus-icon.png" alt="plus-icon" />
       </div>
       <NavItem icon="chat" text="chat" link="/chat" />
       <NavItem icon="user" text="profile" link="/profile" />
