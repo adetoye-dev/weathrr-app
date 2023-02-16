b160085f251e4c39b9a063913e158747ec853971
Adetoye Adewoye <71382301+adetoye-dev@users.noreply.github.com>
2023-02-16 16:38:31 +0100

    Render Auth Context provider

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,14 @@ import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import WeatherContext from "./contexts/WeatherContext";
+import AuthContextProvider from "./contexts/AuthContext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-    <WeatherContext>
-      <App />
-    </WeatherContext>
+    <AuthContextProvider>
+      <WeatherContext>
+        <App />
+      </WeatherContext>
+    </AuthContextProvider>
   </BrowserRouter>
 );
