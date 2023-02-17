0a847022e252089971181ab8860b52d1e39558be
Adetoye Adewoye <71382301+adetoye-dev@users.noreply.github.com>
2023-02-17 11:40:51 +0100

    Refactor postCard props handling

--- src/components/posts/PostCard.jsx
+++ src/components/posts/PostCard.jsx
@@ -2,12 +2,18 @@ import "./PostCard.css";
 import profileIcon from "../../assets/profile-icon.png";
 import { useNavigate } from "react-router-dom";
 
-const PostCard = (props) => {
+const PostCard = ({ postData }) => {
   const navigate = useNavigate();
 
   const viewPost = () => {
-    navigate(`/posts/${props.title}`, {
-      state: { ...props },
+    navigate(`/posts/${postData.title}`, {
+      state: { ...postData },
+    });
+  };
+
+  const viewProfile = (id) => {
+    navigate(`/profile/${id}`, {
+      state: {},
     });
   };
 
@@ -16,33 +22,33 @@ const PostCard = (props) => {
       <div className="favorite-icon">
         <img
           src={`/icons/${
-            props.favorite ? "favorite-active.svg" : "favorite.svg"
+            postData.favorite ? "favorite-active.svg" : "favorite.svg"
           }`}
           alt="fav-icon"
         />
       </div>
       <div className="post-img" onClick={viewPost}>
-        <img src={`/images/${props.postImage}`} alt="post-image" />
+        <img src={`/images/${postData.postImage}`} alt="post-image" />
       </div>
       <div className="user-info flex-center-y">
-        <div className="user-img">
+        <div className="user-img" onClick={() => viewProfile(postData.userId)}>
           <img src={profileIcon} alt="user" />
         </div>
-        <span className="user-name">{props.userName}</span>
+        <span className="user-name">{postData.userName}</span>
         <div className="weather-info flex-center-y">
-          <span className="temp">{props.temp}</span>
+          <span className="temp">{postData.temp}</span>
           <span className="icon">
-            <img src="./icons/clear sky.svg" />
+            <img src="/icons/clear sky.svg" />
           </span>
         </div>
       </div>
       <div className="post-info flex-center-y">
-        <h5 className="post-title">{props.title}</h5>
+        <h5 className="post-title">{postData.title}</h5>
         <div className="location-info flex-center-y">
           <span className="icon">
             <img src="/icons/location-icon.svg" alt="location-icon" />
           </span>
-          <span className="location">{props.location}</span>
+          <span className="location">{postData.location}</span>
         </div>
       </div>
     </div>
