4611613c1a9d41a6f11040aa00e40f839abae6e4
Adetoye Adewoye <71382301+adetoye-dev@users.noreply.github.com>
2023-02-17 13:56:36 +0100

    Make primary nav responsive

--- src/components/navigations/PrimaryNav.jsx
+++ src/components/navigations/PrimaryNav.jsx
@@ -1,13 +1,19 @@
 import "./PrimaryNav.css";
-import profileIcon from "../../assets/profile-icon.png";
+import { useState } from "react";
 import logo from "../../assets/logo.svg";
 import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 import { useUserData } from "../../contexts/AuthContext";
 
 const PrimaryNav = () => {
+  const [dataAttributes, setDataAttributes] = useState(false);
+
   const navigate = useNavigate();
   const { currentUser } = useUserData();
+
+  const toggleNavView = () => {
+    setDataAttributes((prevAttr) => !prevAttr);
+  };
 
   const viewProfile = () => {
     navigate(`/profile/${currentUser.id}`, {
@@ -18,22 +24,38 @@ const PrimaryNav = () => {
   return (
     <header className="header">
       <div className="container nav-container">
-        <ul className="nav-list">
-          <li className="nav-list-item">
-            <Link to="/">Lookbook</Link>
-          </li>
-          <li className="nav-list-item">
-            <Link to="/weather">Weather</Link>
-          </li>
-          <li className="nav-list-item">
-            <Link to="/chat">Chat</Link>
-          </li>
-        </ul>
+        <nav className="nav">
+          <ul
+            className="primary-navigation"
+            data-visible={dataAttributes}
+            id="primary-navigation"
+          >
+            <div onClick={toggleNavView} className="nav-icon cancel-btn">
+              <img src="/icons/close-btn.svg" alt="close-btn" />
+            </div>
+            <li className="nav-list-item">
+              <Link to="/">Lookbook</Link>
+            </li>
+            <li className="nav-list-item">
+              <Link to="/weather">Weather</Link>
+            </li>
+            <li className="nav-list-item">
+              <Link to="/chat">Chat</Link>
+            </li>
+          </ul>
+          <div
+            aria-controls="primary-navigation"
+            onClick={toggleNavView}
+            className="nav-icon menu-btn"
+          >
+            <img src="/icons/menu-btn.svg" alt="menu-btn" />
+          </div>
+        </nav>
         <div className="logo">
           <img src={logo} alt="logo" />
         </div>
         <div onClick={viewProfile} className="profile-icon">
-          <img src={currentUser.profilePic} />
+          <img src={currentUser.profilePic} alt="profile-pic" />
         </div>
       </div>
     </header>
