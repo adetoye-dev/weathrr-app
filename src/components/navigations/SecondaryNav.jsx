7502e7c5d5b47d7e259fe3d3d4002b5e79394a5a
Adetoye Adewoye <71382301+adetoye-dev@users.noreply.github.com>
2023-02-17 13:11:19 +0100

    Set navigate for secondary nav profile page

--- src/components/navigations/SecondaryNav.jsx
+++ src/components/navigations/SecondaryNav.jsx
@@ -1,7 +1,18 @@
 import "./SecondaryNav.css";
 import NavItem from "./NavItem";
+import { useNavigate } from "react-router-dom";
+import { useUserData } from "../../contexts/AuthContext";
 
 const SecondaryNav = () => {
+  const navigate = useNavigate();
+  const { currentUser } = useUserData();
+
+  const viewProfile = () => {
+    navigate(`/profile/${currentUser.id}`, {
+      state: { ...currentUser },
+    });
+  };
+
   return (
     <div className="secondary-nav">
       <NavItem icon="home" text="home" link="/" />
@@ -10,7 +21,15 @@ const SecondaryNav = () => {
         <img src="/icons/plus-icon.png" alt="plus-icon" />
       </div>
       <NavItem icon="chat" text="chat" link="/chat" />
-      <NavItem icon="user" text="profile" link="/profile" />
+      <div className="secondary-nav-item" onClick={viewProfile}>
+        <div
+          className="nav-icon"
+          style={{
+            background: "url(/icons/user.svg) center center no-repeat",
+          }}
+        ></div>
+        <span className="nav-text">Profile</span>
+      </div>
     </div>
   );
 };
