f41002af623c12e1839b172b852bbfef09194e25
Adetoye Adewoye <71382301+adetoye-dev@users.noreply.github.com>
2023-02-22 20:58:45 +0100

    Refactor nav items to use NavLink

--- src/components/navigations/NavItem.jsx
+++ src/components/navigations/NavItem.jsx
@@ -1,14 +1,14 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 const NavItem = ({ icon, text, link, handleClick }) => {
   return (
     <li className="nav-list-item" onClick={handleClick}>
-      <Link to={link} className="nav-link">
+      <NavLink to={link} className="nav-link">
         <span className="nav-icon">
           <img src={`/icons/${icon}`} alt="nav-icon" />
         </span>
         <span className="nav-text">{text}</span>
-      </Link>
+      </NavLink>
     </li>
   );
 };
