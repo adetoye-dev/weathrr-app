6f4eded3a0100a4185c71ca397b8b711d6576efb
Adetoye Adewoye <71382301+adetoye-dev@users.noreply.github.com>
2023-02-22 21:08:32 +0100

    use active class in navlinks

--- src/components/navigations/NavItem.jsx
+++ src/components/navigations/NavItem.jsx
@@ -4,10 +4,18 @@ const NavItem = ({ icon, text, link, handleClick }) => {
   return (
     <li className="nav-list-item" onClick={handleClick}>
       <NavLink to={link} className="nav-link">
-        <span className="nav-icon">
-          <img src={`/icons/${icon}`} alt="nav-icon" />
-        </span>
-        <span className="nav-text">{text}</span>
+        {({ isActive }) => (
+          <>
+            <span className="nav-icon">
+              <img src={`/icons/${icon}`} alt="nav-icon" />
+            </span>
+            <span
+              className={isActive ? "nav-link-active nav-text" : "nav-text"}
+            >
+              {text}
+            </span>
+          </>
+        )}
       </NavLink>
     </li>
   );
