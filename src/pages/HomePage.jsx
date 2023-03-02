50912cbbe4e681372a930d093db1caaaa00370f3
Adetoye Adewoye <71382301+adetoye-dev@users.noreply.github.com>
2023-03-02 09:05:07 +0100

    Fetch posts from server with queryClient

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,16 +1,25 @@
 import PostCard from "../components/posts/PostCard";
 import MasonryLayout from "./layouts/MasonryLayout";
-import data from "../../data.json";
+import postData from "../../data.json";
+import server from "../apis/server";
+import { useQuery } from "@tanstack/react-query";
 
 const HomePage = () => {
+  const { isLoading, error, data } = useQuery({
+    queryKey: ["posts"],
+    queryFn: () => server.get("/posts").then((res) => res.data),
+  });
+
+  console.log(data);
+
   return (
-    <>
-      <MasonryLayout>
-        {data.map((item, index) => {
-          return <PostCard key={index} postData={item} />;
-        })}
-      </MasonryLayout>
-    </>
+    <MasonryLayout>
+      {isLoading
+        ? "Fetching posts..."
+        : data.map((item, index) => {
+            return <PostCard key={index} postData={item} />;
+          })}
+    </MasonryLayout>
   );
 };
 
--- src/pages/layouts/PageLayout.jsx
+++ src/pages/layouts/PageLayout.jsx
@@ -1,14 +1,17 @@
 import PrimaryNav from "../../components/navigations/PrimaryNav";
 import { Outlet } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const PageLayout = () => {
+  const queryClient = new QueryClient();
+
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <PrimaryNav />
       <main className="container content-container">
         <Outlet />
       </main>
-    </>
+    </QueryClientProvider>
   );
 };
 
