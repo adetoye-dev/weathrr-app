56f390412f08dde66bb2a652ab48365f113bd3d5
Adetoye Adewoye <71382301+adetoye-dev@users.noreply.github.com>
2023-03-05 14:52:16 +0100

    Create bookmarks page

---
+++ src/pages/BookmarkPage.jsx
@@ -0,0 +1,9 @@
+const BookmarkPage = () => {
+  return (
+    <div className="bookmarks" style={{ textAlign: "center" }}>
+      <h3>No saved posts.</h3>
+    </div>
+  );
+};
+
+export default BookmarkPage;
