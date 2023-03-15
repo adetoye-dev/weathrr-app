046d034361d23ff020154c96fe25852230d28cbc
Adetoye Adewoye <71382301+adetoye-dev@users.noreply.github.com>
2023-03-15 13:53:27 +0100

    Update profile card design

--- src/components/profile/ProfileCard.jsx
+++ src/components/profile/ProfileCard.jsx
@@ -5,31 +5,27 @@ const ProfileCard = () => {
   const { currentUser } = useUserData();
   return (
     <>
-      <h1 className="user-name">{currentUser.name}</h1>
       <div className="profile-card">
         <div className="profile-image">
           <img src={currentUser.profilePic} alt="profile-img" />
         </div>
+        <h1 className="profile-name">{currentUser.name}</h1>
         <div className="profile-data">
-          <div className="single-post-location-card">
-            <div className="post-location-info">
-              <span className="icon">
-                <img src="/icons/location-icon.png" alt="location-icon" />
-              </span>
-              <span className="post-location">location</span>
-            </div>
-            <div className="post-weather-info">
-              <span className="temp">15</span>
-              <span className="icon">
-                <img src="/icons/clear sky.svg" />
-              </span>
-            </div>
+          <div className="post-location-info">
+            <span className="icon">
+              <img src="/icons/location-icon.png" alt="location-icon" />
+            </span>
+            <span className="post-location">location</span>
           </div>
-          <div className="about-user">
-            nsectetur adipiscing elit, sed do eiusmod tempor incididunt u
-          </div>
-          <button className="send-message-btn">Message</button>
         </div>
+        <div className="about-user">
+          nsectetur adipiscing elit, sed do eiusmod tempor incididunt u
+        </div>
+        <div className="follow-metrics">
+          <span className="follower-count">1k followers</span>
+          <span className="following-count">1.5k following</span>
+        </div>
+        <button className="follow-user-btn">Follow</button>
       </div>
     </>
   );
