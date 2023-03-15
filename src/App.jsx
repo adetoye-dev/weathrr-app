c6474b15999ea96fe6be3561acb72c5a883cd790
Adetoye Adewoye <71382301+adetoye-dev@users.noreply.github.com>
2023-03-15 10:07:47 +0100

    Update app routes and components

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,11 @@ import "./App.css";
 import PageLayout from "./pages/layouts/PageLayout";
 import WeatherPage from "./pages/WeatherPage";
 import HomePage from "./pages/HomePage";
-import ChatPage from "./pages/ChatPage";
 import ProfilePage from "./pages/ProfilePage";
 import BookmarkPage from "./pages/BookmarkPage";
 import Login from "./pages/auth/Login";
 import SignUp from "./pages/auth/SignUp";
+import Logout from "./pages/auth/Logout";
 import { Routes, Route, Navigate } from "react-router-dom";
 import { useUserData } from "./contexts/AuthContext";
 import SinglePostPage from "./pages/SinglePostPage";
@@ -34,7 +34,7 @@ const App = () => {
         <Route index element={<HomePage />} />
         <Route path="posts/:id" element={<SinglePostPage />} />
         <Route path="weather" element={<WeatherPage />} />
-        <Route path="chat" element={<ChatPage />} />
+        <Route path="logout" element={<Logout />} />
         <Route path="profile/:id" element={<ProfilePage />} />
         <Route path="bookmarks" element={<BookmarkPage />} />
       </Route>
