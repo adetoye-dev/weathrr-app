57df927f37e68e46af0e25a7f4ce3c9d4996cef0
Adetoye Adewoye <71382301+adetoye-dev@users.noreply.github.com>
2023-03-16 12:57:55 +0100

    Add MobileNav and AddPost Modal to layout

--- src/pages/layouts/PageLayout.jsx
+++ src/pages/layouts/PageLayout.jsx
@@ -1,6 +1,8 @@
 import PrimaryNav from "../../components/navigations/PrimaryNav";
+import MobileNav from "../../components/navigations/MobileNav";
 import { Outlet } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import AddPost from "../../components/posts/addPost";
 
 const PageLayout = () => {
   const queryClient = new QueryClient();
@@ -9,8 +11,10 @@ const PageLayout = () => {
     <QueryClientProvider client={queryClient}>
       <PrimaryNav />
       <main className="container content-container">
+        <AddPost />
         <Outlet />
       </main>
+      <MobileNav />
     </QueryClientProvider>
   );
 };
