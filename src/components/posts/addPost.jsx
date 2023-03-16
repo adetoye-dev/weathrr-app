781e621c8dd3b33fa90fdd10d57f886a8c2121e8
Adetoye Adewoye <71382301+adetoye-dev@users.noreply.github.com>
2023-03-16 12:24:03 +0100

    Create AddPost modal

---
+++ src/components/posts/addPost.jsx
@@ -0,0 +1,40 @@
+import { useState } from "react";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
+import { useAddPost } from "../../contexts/AddPostContext";
+
+const AddPost = () => {
+  const { open, handleClose } = useAddPost();
+
+  return (
+    <Dialog open={open} onClose={handleClose}>
+      <DialogTitle>New Post</DialogTitle>
+      <DialogContent>
+        <DialogContentText>
+          To subscribe to this website, please enter your email address here. We
+          will send updates occasionally.
+        </DialogContentText>
+        <TextField
+          autoFocus
+          margin="dense"
+          id="name"
+          label="Email Address"
+          type="email"
+          fullWidth
+          variant="standard"
+        />
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={handleClose}>Cancel</Button>
+        <Button onClick={handleClose}>Post</Button>
+      </DialogActions>
+    </Dialog>
+  );
+};
+
+export default AddPost;
