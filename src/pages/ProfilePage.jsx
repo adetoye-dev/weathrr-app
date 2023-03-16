d50d8ab5779d091da55145640423f767d1ebe76c
Adetoye Adewoye <71382301+adetoye-dev@users.noreply.github.com>
2023-03-16 09:19:47 +0100

    Update profile page to fetch data from server

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -1,22 +1,55 @@
+import { useState, useEffect } from "react";
 import ProfileCard from "../components/profile/ProfileCard";
 import "./ProfilePage.css";
 import MasonryLayout from "./layouts/MasonryLayout";
+import server from "../apis/server";
+import { useQuery } from "@tanstack/react-query";
 import PostCard from "../components/posts/PostCard";
 import data from "../../data.json";
 import { useLocation } from "react-router-dom";
 
 const ProfilePage = () => {
   const { state } = useLocation();
+  console.log({ state: state });
+  const [user, setUser] = useState(null);
+  const [posts, setPosts] = useState();
+  console.log(state);
+  const { isLoading, error, data } = useQuery({
+    queryKey: ["users"],
+    queryFn: () =>
+      server.get(`/users/${state.userId}`).then((res) => {
+        setUser(res.data[0]);
+        return res.data;
+      }),
+  });
+
+  useEffect(() => {
+    const fetchUserPosts = async () => {
+      const posts = await server
+        .get(`/posts/${user.id}`)
+        .then((res) => res.data);
+      setPosts(posts);
+    };
+    if (user) {
+      fetchUserPosts();
+    }
+  }, [user]);
+
+  console.log(data, user);
   return (
     <>
-      <ProfileCard />
+      {user && <ProfileCard user={user} />}
       <div className="user-posts">
         <div className="posts-title">Posts</div>
-        <MasonryLayout>
-          {data.map((item, index) => {
-            return <PostCard key={index} postData={item} />;
-          })}
-        </MasonryLayout>
+        {posts && posts.length ? (
+          <MasonryLayout>
+            {posts.map((item, index) => {
+              return <PostCard key={index} postData={item} />;
+            })}
+          </MasonryLayout>
+        ) : (
+          "No posts yet"
+        )}
       </div>
     </>
   );
