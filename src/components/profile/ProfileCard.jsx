d6da620d12351375a01ac156de7860f0d83c19ba
Adetoye Adewoye <71382301+adetoye-dev@users.noreply.github.com>
2023-03-16 09:18:27 +0100

    Update ProfileCard props

--- src/components/profile/ProfileCard.jsx
+++ src/components/profile/ProfileCard.jsx
@@ -1,29 +1,27 @@
 import "./ProfileCard.css";
-import { useUserData } from "../../contexts/AuthContext";
 
-const ProfileCard = () => {
-  const { currentUser } = useUserData();
+const ProfileCard = ({ user }) => {
   return (
     <>
       <div className="profile-card">
         <div className="profile-image">
-          <img src={currentUser.profilePic} alt="profile-img" />
+          <img src={user.profilePic} alt="profile-img" />
         </div>
-        <h1 className="profile-name">{currentUser.name}</h1>
+        <h1 className="profile-name">{user.name}</h1>
         <div className="profile-data">
           <div className="post-location-info">
             <span className="icon">
               <img src="/icons/location-icon.png" alt="location-icon" />
             </span>
-            <span className="post-location">location</span>
+            <span className="post-location">{user.city}</span>
           </div>
         </div>
         <div className="about-user">
           nsectetur adipiscing elit, sed do eiusmod tempor incididunt u
         </div>
         <div className="follow-metrics">
-          <span className="follower-count">1k followers</span>
-          <span className="following-count">1.5k following</span>
+          <span className="follower-count">0 followers</span>
+          <span className="following-count">0 following</span>
         </div>
         <button className="follow-user-btn">Follow</button>
       </div>
