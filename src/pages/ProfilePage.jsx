f43087869498ffa151dc2793f0563815ea15bfca
Adetoye Adewoye <71382301+adetoye-dev@users.noreply.github.com>
2023-03-21 19:58:27 +0100

    Refactor profile fetch to use mutation

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -1,56 +1,50 @@
-import { useState, useEffect } from "react";
+import { useEffect } from "react";
 import ProfileCard from "../components/profile/ProfileCard";
 import "./ProfilePage.css";
 import MasonryLayout from "./layouts/MasonryLayout";
 import server from "../apis/server";
-import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
+import { useMutation } from "@tanstack/react-query";
 import PostCard from "../components/posts/PostCard";
 import { useLocation } from "react-router-dom";
 
 const ProfilePage = () => {
   const { state } = useLocation();
 
-  const queryClient = useQueryClient();
-
-  const {
-    isLoading,
-    error,
-    data: user,
-  } = useQuery({
-    queryKey: ["users"],
-    queryFn: () =>
+  const userMutation = useMutation({
+    mutationFn: () =>
       server.get(`/users/${state.userId}`).then((res) => {
         return res.data[0];
       }),
   });
 
-  const {
-    isLoading: loadingPosts,
-    error: postsError,
-    data: posts,
-  } = useQuery({
-    queryKey: ["posts"],
-    queryFn: () =>
+  const postMutation = useMutation({
+    mutationFn: () =>
       server.get(`/posts/${state.userId}`).then((res) => {
         return res.data;
       }),
   });
 
-  console.log(user);
+  useEffect(() => {
+    userMutation.mutate();
+    postMutation.mutate();
+  }, [state.userId]);
+
   return (
     <>
-      {isLoading ? (
+      {userMutation.isLoading ? (
         "Loading profile"
+      ) : userMutation.isError ? (
+        <p>Unable to fetch user, Kindly check your connection and retry!</p>
       ) : (
         <>
-          {user && <ProfileCard user={user} />}
+          {userMutation.data && <ProfileCard user={userMutation.data} />}
           <div className="user-posts">
             <div className="posts-title">Posts</div>
-            {loadingPosts ? (
+            {postMutation.isLoading ? (
               "Loading posts"
-            ) : posts && posts.length ? (
+            ) : postMutation.data && postMutation.data.length ? (
               <MasonryLayout>
-                {posts.map((item, index) => {
+                {postMutation.data.map((item, index) => {
                   return <PostCard key={index} postData={item} />;
                 })}
               </MasonryLayout>
