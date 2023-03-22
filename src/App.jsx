bc5e2013ad24ec268512400b8c0e3847efe3678a
Adetoye Adewoye <71382301+adetoye-dev@users.noreply.github.com>
2023-03-22 21:25:08 +0100

    Lazy load page routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
 import "./App.css";
+import { lazy, Suspense } from "react";
 import PageLayout from "./pages/layouts/PageLayout";
-import WeatherPage from "./pages/WeatherPage";
-import HomePage from "./pages/HomePage";
-import ProfilePage from "./pages/ProfilePage";
-import BookmarkPage from "./pages/BookmarkPage";
 import Login from "./pages/auth/Login";
 import SignUp from "./pages/auth/SignUp";
-import Logout from "./pages/auth/Logout";
 import { Routes, Route, Navigate } from "react-router-dom";
 import { useUserData } from "./contexts/AuthContext";
-import SinglePostPage from "./pages/SinglePostPage";
+import Loader from "./components/loader/Loader";
+
+const HomePage = lazy(() => import("./pages/HomePage"));
+const WeatherPage = lazy(() => import("./pages/WeatherPage"));
+const ProfilePage = lazy(() => import("./pages/ProfilePage"));
+const BookmarkPage = lazy(() => import("./pages/BookmarkPage"));
+const Logout = lazy(() => import("./pages/auth/Logout"));
+const SinglePostPage = lazy(() => import("./pages/SinglePostPage"));
 
 const Protected = ({ currentUser, children }) => {
   if (!currentUser) {
@@ -31,12 +34,54 @@ const App = () => {
           </Protected>
         }
       >
-        <Route index element={<HomePage />} />
-        <Route path="posts/:id" element={<SinglePostPage />} />
-        <Route path="weather" element={<WeatherPage />} />
-        <Route path="logout" element={<Logout />} />
-        <Route path="profile/:id" element={<ProfilePage />} />
-        <Route path="bookmarks" element={<BookmarkPage />} />
+        <Route
+          index
+          element={
+            <Suspense fallback={<Loader loaderText="Loading..." />}>
+              <HomePage />
+            </Suspense>
+          }
+        />
+        <Route
+          path="posts/:id"
+          element={
+            <Suspense fallback={<Loader loaderText="Loading..." />}>
+              <SinglePostPage />
+            </Suspense>
+          }
+        />
+        <Route
+          path="weather"
+          element={
+            <Suspense fallback={<Loader loaderText="Loading..." />}>
+              <WeatherPage />
+            </Suspense>
+          }
+        />
+        <Route
+          path="logout"
+          element={
+            <Suspense fallback={<Loader loaderText="Loading..." />}>
+              <Logout />
+            </Suspense>
+          }
+        />
+        <Route
+          path="profile/:id"
+          element={
+            <Suspense fallback={<Loader loaderText="Loading..." />}>
+              <ProfilePage />
+            </Suspense>
+          }
+        />
+        <Route
+          path="bookmarks"
+          element={
+            <Suspense fallback={<Loader loaderText="Loading..." />}>
+              <BookmarkPage />
+            </Suspense>
+          }
+        />
       </Route>
       <Route path="login" element={<Login />} />
       <Route path="sign-up" element={<SignUp />} />
