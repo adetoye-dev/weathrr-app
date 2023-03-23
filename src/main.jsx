37ac876b818a821b1db5d144ab02b10498d095ca
Adetoye Adewoye <71382301+adetoye-dev@users.noreply.github.com>
2023-03-23 18:11:14 +0100

    Use update profile context in main

--- src/main.jsx
+++ src/main.jsx
@@ -6,14 +6,17 @@ import "./index.css";
 import AuthContextProvider from "./contexts/AuthContext";
 import WeatherContextProvider from "./contexts/WeatherContext";
 import AddPostContextProvider from "./contexts/AddPostContext";
+import UpdateProfileContextProvider from "./contexts/UpdateProfileContext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <AuthContextProvider>
       <WeatherContextProvider>
-        <AddPostContextProvider>
-          <App />
-        </AddPostContextProvider>
+        <UpdateProfileContextProvider>
+          <AddPostContextProvider>
+            <App />
+          </AddPostContextProvider>
+        </UpdateProfileContextProvider>
       </WeatherContextProvider>
     </AuthContextProvider>
   </BrowserRouter>
