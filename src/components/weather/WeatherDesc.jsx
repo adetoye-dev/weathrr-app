44489c4fadb41c454457e67c5167aa095694c8d0
Adetoye Adewoye <71382301+adetoye-dev@users.noreply.github.com>
2023-03-24 14:34:58 +0100

    Create weather desc card

---
+++ src/components/weather/WeatherDesc.jsx
@@ -0,0 +1,20 @@
+import { useWeatherData } from "../../contexts/WeatherContext";
+import "./WeatherDesc.css";
+
+const WeatherDesc = () => {
+  const { weatherData } = useWeatherData();
+  return (
+    <>
+      {weatherData.main ? (
+        <p className="weather-desc">
+          {`Current weather in ${weatherData.name} is ${weatherData.main.temp}°, wind ${weatherData.wind.speed} m/s, feels like
+      ${weatherData.main.feels_like}°. Have a nice day!`}
+        </p>
+      ) : (
+        "Fetching weather data..."
+      )}
+    </>
+  );
+};
+
+export default WeatherDesc;
