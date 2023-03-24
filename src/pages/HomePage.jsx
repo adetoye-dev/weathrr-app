50e1a6704346aeb43b6e9f106644a3e38471fe07
Adetoye Adewoye <71382301+adetoye-dev@users.noreply.github.com>
2023-03-24 14:38:36 +0100

    Render weather desc on home page

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,11 +1,13 @@
 import Posts from "../components/posts/Posts";
 import SearchBar from "../components/search/PostSearch";
 import FilterBox from "../components/search/FilterPosts";
+import WeatherDesc from "../components/weather/WeatherDesc";
 import "./HomePage.css";
 
 const HomePage = () => {
   return (
     <>
+      <WeatherDesc />
       <div className="search-container">
         <SearchBar />
         <FilterBox />
