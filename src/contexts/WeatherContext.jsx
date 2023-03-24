fdef65ef4072e71565b684c1d904e3838fefa5bd
Adetoye Adewoye <71382301+adetoye-dev@users.noreply.github.com>
2023-03-24 14:37:19 +0100

    Update weather context Update weather context to fetch weather on page load

--- src/contexts/WeatherContext.jsx
+++ src/contexts/WeatherContext.jsx
@@ -1,4 +1,6 @@
-import React, { useContext, useState, useMemo } from "react";
+import React, { useContext, useState, useMemo, useEffect } from "react";
+import fetchUserCity from "../apis/ipapi";
+import openweathermap from "../apis/openweathermap";
 
 const weatherContext = React.createContext();
 
@@ -8,10 +10,37 @@ export const useWeatherData = () => {
 
 const WeatherContextProvider = ({ children }) => {
   const [weatherData, setWeatherData] = useState({});
+  const [weatherFetchError, setError] = useState(null);
+
+  //function to load current all weather data
+  const getWeatherData = async (city) => {
+    setError(null);
+    try {
+      const response = await openweathermap.get("/weather", {
+        params: {
+          q: city,
+        },
+      });
+
+      console.log(response.data);
+      setWeatherData(response.data);
+    } catch (err) {
+      setError(err.response.data.message);
+    }
+  };
+
+  useEffect(() => {
+    //get user's city with ipapi and fetch weather data with city value
+    const getCity = async () => {
+      const cityData = await fetchUserCity();
+      getWeatherData(cityData.city);
+    };
+    getCity();
+  }, []);
 
   const contextValues = useMemo(
-    () => [weatherData, setWeatherData],
-    [weatherData]
+    () => ({ weatherData, getWeatherData, weatherFetchError }),
+    [weatherData, weatherFetchError]
   );
 
   return (
