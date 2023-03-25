22b62b8e1260cb3dfef47bf91815cb8c7115b7a8
Adetoye Adewoye <71382301+adetoye-dev@users.noreply.github.com>
2023-03-25 01:43:21 +0100

    Fetch Recommended posts based on user's location

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,9 +2,54 @@ import Posts from "../components/posts/Posts";
 import SearchBar from "../components/search/PostSearch";
 import FilterBox from "../components/search/FilterPosts";
 import WeatherDesc from "../components/weather/WeatherDesc";
+import { useWeatherData } from "../contexts/WeatherContext";
+import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
+import server from "../apis/server";
 import "./HomePage.css";
+import { useEffect } from "react";
 
 const HomePage = () => {
+  const { weatherData } = useWeatherData();
+
+  const queryClient = useQueryClient();
+
+  const city = weatherData?.name;
+
+  const { isLoading, error, data } = useQuery({
+    queryKey: ["posts"],
+    queryFn: async () => {
+      return server
+        .post("posts/recommend", {
+          weather: "",
+          location: weatherData.name,
+          country: weatherData.sys.country,
+        })
+        .then((res) => res.data);
+    },
+    enabled: !!city,
+  });
+
+  const mutation = useMutation({
+    mutationFn: async () => {
+      return server
+        .post("posts/recommend", {
+          weather: "",
+          location: weatherData.name,
+          country: weatherData.sys.country,
+        })
+        .then((res) => res.data);
+    },
+    onSuccess: (data) => {
+      queryClient.setQueryData(["posts"], data);
+    },
+  });
+
+  useEffect(() => {
+    if (weatherData.name) {
+      mutation.mutate();
+    }
+  }, [weatherData]);
+
   return (
     <>
       <WeatherDesc />
@@ -12,7 +57,7 @@ const HomePage = () => {
         <SearchBar />
         <FilterBox />
       </div>
-      <Posts />
+      <Posts isLoading={isLoading} error={error} data={data} />
     </>
   );
 };
