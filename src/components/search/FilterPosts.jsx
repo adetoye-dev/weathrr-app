064cd47426e88e3dab0ee5dab96073363380da70
Adetoye Adewoye <71382301+adetoye-dev@users.noreply.github.com>
2023-03-26 19:18:11 +0100

    Fetch posts based on selected filter

--- src/components/search/FilterPosts.jsx
+++ src/components/search/FilterPosts.jsx
@@ -1,17 +1,60 @@
 import "./FilterPosts.css";
+import server from "../../apis/server";
+import { useWeatherData } from "../../contexts/WeatherContext";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 
-const FilterBox = ({ handleChange }) => {
+const FilterBox = () => {
+  const { weatherData } = useWeatherData();
+
+  const queryClient = useQueryClient();
+
+  const mutation = useMutation({
+    mutationFn: async (filter) => {
+      switch (filter) {
+        case "all":
+          return server.get(`/posts`).then((res) => res.data);
+        case "recommend":
+          return server
+            .post("posts/recommend", {
+              weather: "",
+              location: weatherData.name,
+              country: weatherData.sys.country,
+            })
+            .then((res) => res.data);
+        case "nearby":
+          return server
+            .post("posts/region", {
+              country: weatherData.sys.country,
+            })
+            .then((res) => res.data);
+        case "following":
+          return server.get(`/posts/following`).then((res) => res.data);
+        default:
+          throw new Error(`Invalid filter "${filter}"`);
+      }
+    },
+    onSuccess: (data) => {
+      // Update posts with mutation data
+      queryClient.setQueryData(["posts"], data);
+    },
+  });
+
+  const handleChange = (filter) => {
+    mutation.mutate(filter);
+  };
+
   return (
     <select
       name="filter"
-      //   onChange={(e) => handleChange(e.target.value)}
+      onChange={(e) => handleChange(e.target.value)}
       className="filter-box"
     >
-      <option value="">Filter By</option>
-      <option value="recent">Recent</option>
+      <option value="recommend" selected>
+        For You
+      </option>
       <option value="nearby">Nearby</option>
       <option value="following">Following</option>
-      <option value="popular">Popular</option>
+      <option value="all">All Posts</option>
     </select>
   );
 };
