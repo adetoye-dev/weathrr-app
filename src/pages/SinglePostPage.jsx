b56ed3482286ebbdcb46d630146cdb9344abaab2
Adetoye Adewoye <71382301+adetoye-dev@users.noreply.github.com>
2023-04-10 21:01:55 +0100

    Remove unused code

--- src/pages/SinglePostPage.jsx
+++ src/pages/SinglePostPage.jsx
@@ -3,7 +3,7 @@ import { useLocation } from "react-router-dom";
 
 const SinglePostPage = () => {
   const { state } = useLocation();
-  console.log(state);
+
   return (
     <>
       <div className="single-post-nav"></div>
