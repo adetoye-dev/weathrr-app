97d9cc4c55ce60d42d77e63f793301f5d4fb27df
Adetoye Adewoye <71382301+adetoye-dev@users.noreply.github.com>
2023-04-12 14:43:12 +0100

    Validate Input fields before submission

--- src/pages/auth/SignUp.jsx
+++ src/pages/auth/SignUp.jsx
@@ -5,14 +5,18 @@ import server from "../../apis/server";
 import "./SignUp.css";
 
 const SignUp = () => {
+  const USER_REGEX = /^[A-z][A-z0-9-_]{3,23}$/;
+  const PWD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/;
+
   const [formInputs, setFormInputs] = useState({
     username: "",
     email: "",
     password: "",
     name: "",
   });
-  const [error, setError] = useState(false);
-  const [errorMsg, setErrorMsg] = useState("");
+  const [errorMsg, setErrorMsg] = useState(null);
+  const [inputNotValid, setInputNotValid] = useState(false);
+  const [success, setSuccess] = useState(null);
 
   const handleChange = (e) => {
     setFormInputs((prev) => ({ ...prev, [e.target.name]: e.target.value }));
@@ -20,12 +24,23 @@ const SignUp = () => {
 
   const register = async (e) => {
     e.preventDefault();
+    const v1 = USER_REGEX.test(formInputs.username);
+    const v2 = PWD_REGEX.test(formInputs.password);
+    if (
+      !v1 ||
+      !v2 ||
+      formInputs.email.length <= 0 ||
+      formInputs.name.length <= 0
+    ) {
+      setInputNotValid(true);
+      return;
+    }
     try {
       const response = await server.post("/auth/register", formInputs);
       console.log(response);
+      setSuccess(response);
     } catch (err) {
       console.log(err);
-      setError(true);
       setErrorMsg(err.response.data);
     }
   };
@@ -36,7 +51,8 @@ const SignUp = () => {
         <img src={Logo} alt="logo" />
       </span>
       <p className="sign-up-text">Create a new account</p>
-      {error ? <p className="err-msg">{errorMsg}</p> : ""}
+      {success ? <p className="success">{errorMsg}</p> : ""}
+      {errorMsg ? <p className="err-msg">{errorMsg}</p> : ""}
       <form className="auth-form">
         <div className="user-auth-details">
           <div className="auth-detail">
@@ -45,10 +61,28 @@ const SignUp = () => {
               id="username"
               name="username"
               type="text"
-              value={formInputs.username}
               placeholder="create a user name"
+              required
+              autoComplete="off"
               onChange={handleChange}
             ></input>
+            {inputNotValid ? (
+              formInputs.username.length === 0 ? (
+                <span className="input-err">This field is required</span>
+              ) : (
+                !USER_REGEX.test(formInputs.username) && (
+                  <span className="input-err">
+                    4 to 24 characters.
+                    <br />
+                    Must begin with a letter.
+                    <br />
+                    Letters, numbers, underscores, hyphens allowed.
+                  </span>
+                )
+              )
+            ) : (
+              ""
+            )}
           </div>
           <div className="auth-detail">
             <label htmlFor="email">Email</label>
@@ -56,10 +90,16 @@ const SignUp = () => {
               id="email"
               name="email"
               type="email"
-              value={formInputs.email}
+              autoComplete="off"
+              required
               placeholder="example@mail.com"
               onChange={handleChange}
             ></input>
+            {inputNotValid && formInputs.email.length === 0 ? (
+              <span className="input-err">This field is required</span>
+            ) : (
+              ""
+            )}
           </div>
           <div className="auth-detail">
             <label htmlFor="password">Password</label>
@@ -67,10 +107,34 @@ const SignUp = () => {
               id="password"
               name="password"
               type="password"
-              value={formInputs.password}
+              autoComplete="off"
+              required
               placeholder="Your password"
               onChange={handleChange}
             ></input>
+            {inputNotValid ? (
+              formInputs.password.length === 0 ? (
+                <span className="input-err">This field is required</span>
+              ) : (
+                !PWD_REGEX.test(formInputs.password) && (
+                  <span className="input-err">
+                    8 to 24 characters.
+                    <br />
+                    Must include uppercase and lowercase letters, a number and a
+                    special character.
+                    <br />
+                    Allowed special characters:
+                    <span aria-label="exclamation mark">!</span>
+                    <span aria-label="at symbol">@</span>
+                    <span aria-label="hashtag">#</span>
+                    <span aria-label="dollar sign">$</span>
+                    <span aria-label="percent">%</span>
+                  </span>
+                )
+              )
+            ) : (
+              ""
+            )}
           </div>
           <div className="auth-detail">
             <label htmlFor="name">Name</label>
@@ -78,10 +142,16 @@ const SignUp = () => {
               id="name"
               name="name"
               type="text"
-              value={formInputs.name}
+              autoComplete="off"
+              required
               placeholder="FirstName LastName"
               onChange={handleChange}
             ></input>
+            {inputNotValid && formInputs.name.length === 0 ? (
+              <span className="input-err">This field is required</span>
+            ) : (
+              ""
+            )}
           </div>
         </div>
         <button className="auth-submit-btn" onClick={register}>
