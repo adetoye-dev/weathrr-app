5e32dcef70b8f711a50695a3b8c6db48642a2500
Adetoye Adewoye <71382301+adetoye-dev@users.noreply.github.com>
2023-04-23 18:58:20 +0100

    create server interceptor

---
+++ src/interceptors/serverInterceptor.js
@@ -0,0 +1,42 @@
+import server from "../apis/server";
+
+let refresh = false;
+
+server.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    const prevRequest = error?.config;
+
+    if (error.response.status === 401 && !refresh) {
+      refresh = true;
+
+      const response = await server.post(
+        "/auth/refresh",
+        {},
+        { withCredentials: true }
+      );
+
+      if (response.status === 200) {
+        server.defaults.headers.common[
+          "Authorization"
+        ] = `Bearer ${response.data["token"]}`;
+
+        return server(prevRequest);
+      }
+    }
+
+    if (error?.response?.status === 403 && !prevRequest?.sent) {
+      prevRequest.sent = true;
+      const { data } = await server.post(
+        "/auth/refresh",
+        {},
+        { withCredentials: true }
+      );
+      console.log(data.token);
+      prevRequest.headers["Authorization"] = `Bearer ${data.token}`;
+      return server(prevRequest);
+    }
+    refresh = false;
+    return Promise.reject(error);
+  }
+);
