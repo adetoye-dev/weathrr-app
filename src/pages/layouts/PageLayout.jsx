7af457323422e0bf9f9c10d1ec075050279354cd
Adetoye Adewoye <71382301+adetoye-dev@users.noreply.github.com>
2023-04-28 10:51:00 +0100

    Render alert modal

--- src/pages/layouts/PageLayout.jsx
+++ src/pages/layouts/PageLayout.jsx
@@ -3,12 +3,14 @@ import MobileNav from "../../components/navigations/MobileNav";
 import { Outlet } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import AddPost from "../../components/posts/addPost";
+import Alert from "../../components/alerts/Alert";
 
 const PageLayout = () => {
   const queryClient = new QueryClient();
 
   return (
     <QueryClientProvider client={queryClient}>
+      <Alert />
       <PrimaryNav />
       <main className="container content-container">
         <AddPost />
